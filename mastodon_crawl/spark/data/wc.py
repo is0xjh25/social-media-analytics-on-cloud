057de07464f684cd9b41b0f4da03a64a8f74e5d6
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-04-25 11:38:37 +1000

    Delete: mastodon crawling

--- mastodon_crawl/spark/data/wc.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkConf, SparkContext
-import re
-
-words= re.split('\W+', open("/tmp/wc.txt").read())
-wordsRDD= sc.parallelize(words)
-countsRDD = wordsRDD.map(lambda w: (w, 1)).reduceByKey(lambda a, b: a + b)
-
-wc = countsRDD.collect()
-print(wc)
