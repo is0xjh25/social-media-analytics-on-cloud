9004ff8ce5165eba6dd2ceae0188d3a1268827c5
gracelovesyah <jiahe3@student.unimelb.edu.au>
2023-05-09 00:05:32 +1000

    changed design

--- web_ccc/static/moving_text.js
+++ web_ccc/static/moving_text.js
@@ -122,3 +122,15 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+var nav = document.querySelector('nav'); // Identify target
+
+window.addEventListener('scroll', function(event) { // To listen for event
+    event.preventDefault();
+
+    if (window.scrollY <= 150) { // Just an example
+        nav.style.backgroundColor = '#000'; // or default color
+    } else {
+        nav.style.backgroundColor = 'transparent';
+    }
+});
