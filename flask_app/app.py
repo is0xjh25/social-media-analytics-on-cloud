b490f4d8fcddc2026aa6f5814435ff93181d7d2d
ubuntu <jonghop@student.unimelb.edu.au>
2023-05-19 11:45:55 +0000

    yaml

--- flask_app/app.py
+++ flask_app/app.py
@@ -21,8 +21,12 @@ from flask_caching import Cache
 from flask_cors import CORS
 from flask_cors import cross_origin
 import threading
+import yaml
 
-
+with open("./data/flask-setting/api.yaml", encoding="UTF-8") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+    couch_password = cfg["COUCH_PASSWORD"]
+    master_node = cfg["MASTER_NODE"]
 
 #Create an app object using the Flask class. 
 app = Flask(__name__)
@@ -37,44 +41,44 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 print(dname)
 
-request_counter = 0
+# request_counter = 0
 
-def reset_counter():
-    global request_counter
-    request_counter = 0
-    # 重新设置定时器
-    threading.Timer(60, reset_counter).start()
+# def reset_counter():
+#     global request_counter
+#     request_counter = 0
+#     # 重新设置定时器
+#     threading.Timer(60, reset_counter).start()
 
 
-reset_counter()
+# reset_counter()
 
-@app.before_request
-def increment_request_counter():
-    global request_counter
-    request_counter += 1
-    print("Received request number:", request_counter)
+# @app.before_request
+# def increment_request_counter():
+#     global request_counter
+#     request_counter += 1
+#     print("Received request number:", request_counter)
 
 
 GLOBAL_STATE = False
 
-@app.route('/status')
-def status():
-    global request_counter
-    global GLOBAL_STATE
-    if request_counter >10:
-        GLOBAL_STATE = True
-    else:
-        GLOBAL_STATE = False
-    return jsonify({'status': GLOBAL_STATE})
+# @app.route('/status')
+# def status():
+#     global request_counter
+#     global GLOBAL_STATE
+#     if request_counter >10:
+#         GLOBAL_STATE = True
+#     else:
+#         GLOBAL_STATE = False
+#     return jsonify({'status': GLOBAL_STATE})
 
 
 @app.route('/db/',methods=['GET'])
 def my_view_function():
     # Connect to CouchDB
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['twitter']
     # Query the view
-    view_result = db.view('_design/agg/_view/gcc-score-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/score-gcc-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     keys = [row['key'] for row in result]
     keys = ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
@@ -99,10 +103,10 @@ def home():
 
 @app.route('/s1/',methods=['GET'])
 def s1():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['twitter']
     # Query the view
-    view_result = db.view('_design/agg/_view/month-agg-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/score-month-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     #keys = [row['key'] for row in result]
     keys = ['2022-02', '2022-03', '2022-04', '2022-05', '2022-06', '2022-07', '2022-08']
@@ -127,7 +131,7 @@ def s1():
     # Render the template with the result
 
     # ---------------------------------------------------------------------------
-    view_result = db.view('_design/agg/_view/hour-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/hour-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     keys = [row['key'] for row in result]
     ave = [row['value']['avg'] for row in result]
@@ -174,7 +178,7 @@ def s1():
     
     chart_data2 = json.dumps(fig, cls=PlotlyJSONEncoder)
     # ---------------------------------------------------------------------------
-    view_result = db.view('_design/agg/_view/dow-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/score-dow-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     keys = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     ave = [row['value']['avg'] for row in result]
@@ -198,42 +202,42 @@ def s1_1():
     return render_template('s1.html')
 
 
-# test
-@app.route('/test1')
-@cross_origin()
-def first_test():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
-    db = couch['twitter']
-    view_result = db.view('_design/agg/_view/gcc-score-view', reduce=True, group=True)
-    result = [{'key': row.key, 'value': row.value} for row in view_result]
-    return jsonify(result)
+# # test
+# @app.route('/test1')
+# @cross_origin()
+# def first_test():
+#     couch = couchdb.Server(f'http://{couch_password}@{master_node}')
+#     db = couch['twitter']
+#     view_result = db.view('_design/happiness_doc/_view/score-gcc-view', reduce=True, group=True)
+#     result = [{'key': row.key, 'value': row.value} for row in view_result]
+#     return jsonify(result)
 
 
-@app.route('/gccWeekend')
-@cross_origin()
-def gccWeekend():
-    couch = couchdb.Server('http://admin:admin@172.26.131.83:5984')
-    db = couch['twitter']
-    view_result = db.view('_design/agg/_view/dow-view', reduce=True, group=True)
-    result = [{'key': row.key, 'value': row.value} for row in view_result]
-    # print(result)
-    return jsonify(result)
+# @app.route('/gccWeekend')
+# @cross_origin()
+# def gccWeekend():
+#     couch = couchdb.Server('http://admin:admin@172.26.128.215:5984')
+#     db = couch['twitter']
+#     view_result = db.view('_design/happiness_doc/_view/score-dow-view', reduce=True, group=True)
+#     result = [{'key': row.key, 'value': row.value} for row in view_result]
+#     # print(result)
+#     return jsonify(result)
 
-@app.route('/gccMonth')
-@cross_origin()
-def gccMonth():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
-    db = couch['twitter']
-    view_result = db.view('_design/agg/_view/month-agg-view', reduce=True, group=True)
-    result = [{'key': row.key, 'value': row.value} for row in view_result]
-    return jsonify(result)
+# @app.route('/gccMonth')
+# @cross_origin()
+# def gccMonth():
+#     couch = couchdb.Server(f'http://{couch_password}@{master_node}')
+#     db = couch['twitter']
+#     view_result = db.view('_design/happiness_doc/_view/score-month-view', reduce=True, group=True)
+#     result = [{'key': row.key, 'value': row.value} for row in view_result]
+#     return jsonify(result)
 
 @app.route('/gccCount')
 @cross_origin()
 def gccCount():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['twitter']
-    view_result = db.view('_design/agg/_view/state-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/state-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     return jsonify(result)
 
@@ -241,20 +245,15 @@ def gccCount():
 @app.route('/gccCount2')
 @cross_origin()
 def gccCount_2():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['twitter']
-    view_result = db.view('_design/agg/_view/gcc-score-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/score-gcc-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     print(result)
     return jsonify(result)
 
 
 
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-GLOBAL_STATE = False
 
 @app.route('/s1_data')
 @cross_origin()
@@ -265,21 +264,24 @@ def combinedData():
         return jsonify({'state': True, 'result_1': None, 'result_2': None, 'result_3': None})
     else:
         try:
-            couch = couchdb.Server('http://admin:admin@localhost:5984')
+            couch = couchdb.Server(f'http://{couch_password}@{master_node}')
             db = couch['twitter']
 
             # 1st view result
-            view_result_1 = db.view('_design/agg/_view/hour-view', reduce=True, group=True)
+            view_result_1 = db.view('_design/happiness_doc/_view/score-hour-view', reduce=True, group=True)
             result_1 = [{'key': row.key, 'value': row.value} for row in view_result_1]
 
             # 2nd view result
-            view_result_2 = db.view('_design/agg/_view/month-agg-view', reduce=True, group=True)
+            view_result_2 = db.view('_design/happiness_doc/_view/score-month-view', reduce=True, group=True)
             result_2 = [{'key': row.key, 'value': row.value} for row in view_result_2]
 
             # 3rd view result
-            view_result_3 = db.view('_design/agg/_view/dow-view', reduce=True, group=True)
+            view_result_3 = db.view('_design/happiness_doc/_view/score-dow-view', reduce=True, group=True)
             result_3 = [{'key': row.key, 'value': row.value} for row in view_result_3]
 
+
+            print(result_1)
+            print(result_2)
             return jsonify({'state': False, 'result_1': result_1, 'result_2': result_2, 'result_3': result_3})
 
         except Exception as e:
@@ -294,13 +296,13 @@ def gcc_all_data():
         return jsonify({'state': True, 'result_1': None, 'result_2': None})
     else:
         try:
-            couch = couchdb.Server('http://admin:admin@localhost:5984')
+            couch = couchdb.Server(f'http://{couch_password}@{master_node}')
             db = couch['twitter']
 
-            state_view_result_1 = db.view('_design/agg/_view/state-view', reduce=True, group=True)
-            state_result_1 = [{'key': row.key, 'value': row.value} for row in state_view_result_1]
-
-            score_view_result_2 = db.view('_design/agg/_view/gcc-score-view', reduce=True, group=True)
+            # state_view_result_1 = db.view('_design/happiness_doc/_view/score-state-view', reduce=True, group=True)
+            # state_result_1 = [{'key': row.key, 'value': row.value} for row in state_view_result_1]
+            state_result_1  = []
+            score_view_result_2 = db.view('_design/happiness_doc/_view/score-gcc-view', reduce=True, group=True)
             state_result_2 = [{'key': row.key, 'value': row.value} for row in score_view_result_2]
 
             return jsonify({'state': False, 'result_1': state_result_1, 'result_2': state_result_2})
@@ -313,7 +315,7 @@ def gcc_all_data():
 @app.route('/mastodon')
 @cross_origin()
 def mastdon():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['mastodon']
     view_result = db.view('_design/happiness_doc/_view/score-hour-view', reduce=True, group=True)
     print(view_result)
@@ -324,7 +326,7 @@ def mastdon():
 @app.route('/mastodon_w')
 @cross_origin()
 def mastdon_week():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['mastodon']
     view_result = db.view('_design/happiness_doc/_view/score-dow-view', reduce=True, group=True)
     print(view_result)
@@ -335,7 +337,7 @@ def mastdon_week():
 @app.route('/mastodon_b')
 @cross_origin()
 def mastdon_b():
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['mastodon']
     view_result = db.view('_design/happiness_doc/_view/behaviour-dow-view', reduce=True, group=True)
     print(view_result)
@@ -361,10 +363,10 @@ def s1_4():
 
 def s2():
     # Connect to CouchDB
-    couch = couchdb.Server('http://admin:admin@localhost:5984')
+    couch = couchdb.Server(f'http://{couch_password}@{master_node}')
     db = couch['twitter']
     # Query the view
-    view_result = db.view('_design/agg/_view/gcc-score-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/score-gcc-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     keys = [row['key'] for row in result]
     ave = [row['value']['avg'] for row in result]
@@ -409,7 +411,7 @@ def s2():
     
 
     # -----------------------------------------------------
-    view_result = db.view('_design/agg/_view/sal-score-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/sal-score-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     keys = [row['key'] for row in result]
     ave = [row['value']['avg'] for row in result]
@@ -435,7 +437,7 @@ def s2():
     c.add_to(m2)
     # -------------------------state-------------------------------------
 
-    view_result = db.view('_design/agg/_view/state-view', reduce=True, group=True)
+    view_result = db.view('_design/happiness_doc/_view/state-view', reduce=True, group=True)
     result = [{'key': row.key, 'value': row.value} for row in view_result]
     ave = [row['value']['avg'] for row in result]
     keys = ['NSW', 'VIC', 'QLD', 'SA', 'WA','TAS', 'NT', 'ACT']
@@ -455,44 +457,6 @@ def s2():
     #state_locations = {'nsw': (-32.0, 146.0), 'vic': (-37.0, 145.0), 'qld': (-20.0, 143.0), 'sa': (-30.0, 135.0), 'wa': (-27.0, 121.0), 'tas': (-42.0, 146.0), 'nt': (-20.0, 133.0), 'act': (-35.0, 149.0)}
     #averages_by_state = {'nsw': 5.939376343261581, 'vic': 5.900216282200858, 'qld': 5.894326677380478, 'sa': 5.924167045178846, 'wa': 5.889941445951669, 'tas': 5.954870757559762, 'nt': 5.929317511101778, 'act': 5.936749708020824}
     return render_template('s2.html', result = result, chart_data=chart_data, chart_data2=chart_data2, map=m._repr_html_(), map2=m2._repr_html_())
-
-@app.route('/s2/#s2.1')
-def s2_1():
-    return render_template('s2.html')
-
-@app.route('/s2/#s2.2')
-def s2_2():
-    return render_template('s2.html')
-@app.route('/s2/#s2.3')
-def s2_3():
-    return render_template('s2.html')
-@app.route('/s2/#s2.4')
-def s2_4():
-    return render_template('s2.html')
-
-#---------------------------s3--------------------------
-@app.route('/s3/')
-def s3():
-    return render_template('s3.html')
-@app.route('/s3/#s3.1')
-def s3_1():
-    return render_template('s3.html')
-
-@app.route('/s3/#s3.2')
-def s3_2():
-    return render_template('s3.html')
-@app.route('/s3/#s3.3')
-def s3_3():
-    return render_template('s3.html')
-@app.route('/s3/#s3.4')
-def s3_4():
-    return render_template('s3.html')
-
-#---------------------------s4--------------------------
-@app.route('/s4/')
-def s4():
-    return render_template('s4.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
