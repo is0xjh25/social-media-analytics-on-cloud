697c7b02f22ac34611d54e2c9b9954d311e593d4
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-05-20 15:18:23 +1000

    Update: few scripts

--- ansible/roles/deploy-backend/deploy-bankend.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker pull jonghop/flask-docker
-docker run -p 5000:5000 --name backend -d flask-docker
--- ansible/roles/deploy-mastodon-harvester/deploy-mastodon-harvester.sh
+++ ansible/roles/deploy-mastodon-harvester/deploy-mastodon-harvester.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo docker pull jonghop/mastodon-docker
-sudo docker run -v /volume/data:/src/mastodon_server/data --name mastodon -d jonghop/mastodon-docker
+sudo docker run -v /volume/data:/app/data -p 5000:5000 -d jonghop/flask-docker
