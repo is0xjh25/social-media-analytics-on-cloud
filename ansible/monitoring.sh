598c38a6ceee2dcfc5a4ae71d0409b216225d948
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2023-05-21 13:25:41 +1000

    Update: crontab script

--- ansible/monitoring.sh
+++ ansible/monitoring.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Monitoring
 ansible-playbook -i ./hosts.ini monitoring.yaml
+
+echo `date` >> /home/ubuntu/ansible/roles/start-monitoring/monitoring-log.txt
--- ansible/roles/deploy-frontend/deploy-frontend.sh
+++ ansible/roles/deploy-frontend/deploy-frontend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull jonghop/react-docker
-docker run -p 3000:3000 --name frontend -d react-docker
+docker run -v ~/data:/app/data -p 3000:3000 --name frontend -d jonghop/react-docker
